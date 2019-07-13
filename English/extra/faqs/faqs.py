bf84aa72ba6186644a90601b951bf3837771d42a
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2019-07-13 17:51:25 -0500

    Update faqs.py

--- English/extra/faqs/faqs.py
+++ English/extra/faqs/faqs.py
@@ -133,23 +133,10 @@ class ChangePositionAndSizeCameraInAnotherScene(GraphScene,MovingCameraScene):
         "y_tick_frequency" : 5, 
         "x_tick_frequency" : 0.5, 
     }
+    # Setup the scenes
     def setup(self):            
-        # You have to copy the setup of the scenes that you use
-        # In this case, the setup from GrapScene and MovingCameraScene
-
-        # This setup is from manimlib/scene/graph_scene.py
-        self.default_graph_colors_cycle = it.cycle(self.default_graph_colors)
-
-        self.left_T_label = VGroup()
-        self.left_v_line = VGroup()
-        self.right_T_label = VGroup()
-        self.right_v_line = VGroup()
-
-        # This setup is from manimlib/scene/moving_camera_scene.py
-        Scene.setup(self)
-        assert(isinstance(self.camera, MovingCamera))
-        self.camera_frame = self.camera.frame
-        return self
+        GraphScene.setup(self)
+        MovingCameraScene.setup(self)
 
     def construct(self):
         self.setup_axes(animate=False)
@@ -233,4 +220,111 @@ class RemoveAllObjectsInScreen(Scene):
             # All mobjects in the screen are saved in self.mobjects
         )
 
+        self.wait()
+# --------------------------------------------------------------------
+# Zoomed Scene
+class ZoomedSceneExample(ZoomedScene):
+    CONFIG = {
+        "zoom_factor": 0.3,
+        "zoomed_display_height": 1,
+        "zoomed_display_width": 6,
+        "image_frame_stroke_width": 20,
+        "zoomed_camera_config": {
+            "default_frame_stroke_width": 3,
+        },
+    }
+
+    def construct(self):
+        # Set objects
+        dot = Dot().shift(UL*2)
+
+        image=ImageMobject(np.uint8([[ 0, 100,30 , 200],
+                                     [255,0,5 , 33]]))
+        image.set_height(7)
+        frame_text=TextMobject("Frame",color=PURPLE).scale(1.4)
+        zoomed_camera_text=TextMobject("Zommed camera",color=RED).scale(1.4)
+
+        self.add(image,dot)
+
+        # Set camera
+        zoomed_camera = self.zoomed_camera
+        zoomed_display = self.zoomed_display
+        frame = zoomed_camera.frame
+        zoomed_display_frame = zoomed_display.display_frame
+
+        frame.move_to(dot)
+        frame.set_color(PURPLE)
+
+        zoomed_display_frame.set_color(RED)
+        zoomed_display.shift(DOWN)
+
+        # brackground zoomed_display
+        zd_rect = BackgroundRectangle(
+            zoomed_display,
+            fill_opacity=0,
+            buff=MED_SMALL_BUFF,
+        )
+
+        self.add_foreground_mobject(zd_rect)
+
+        # animation of unfold camera
+        unfold_camera = UpdateFromFunc(
+            zd_rect,
+            lambda rect: rect.replace(zoomed_display)
+        )
+
+        frame_text.next_to(frame,DOWN)
+
+        self.play(
+            ShowCreation(frame),
+            FadeInFromDown(frame_text)
+        )
+
+        # Activate zooming
+        self.activate_zooming()
+
+        self.play(
+            # You have to add this line
+            self.get_zoomed_display_pop_out_animation(),
+            unfold_camera
+        )
+
+        zoomed_camera_text.next_to(zoomed_display_frame,DOWN)
+        self.play(FadeInFromDown(zoomed_camera_text))
+
+        # Scale in     x   y  z
+        scale_factor=[0.5,1.5,0]
+
+        # Resize the frame and zoomed camera
+        self.play(
+            frame.scale,                scale_factor,
+            zoomed_display.scale,       scale_factor,
+            FadeOut(zoomed_camera_text),
+            FadeOut(frame_text)
+        )
+
+        # Resize the frame
+        self.play(
+            frame.scale,3,
+            frame.shift,2.5*DOWN
+        )
+
+        # Resize zoomed camera
+        self.play(
+            ScaleInPlace(zoomed_display,2)
+        )
+
+
+        self.wait()
+
+        self.play(
+            self.get_zoomed_display_pop_out_animation(),
+            unfold_camera,
+            # -------> Inverse
+            rate_func=lambda t: smooth(1-t),
+        )
+        self.play(
+            Uncreate(zoomed_display_frame),
+            FadeOut(frame),
+        )
         self.wait()
