e1737d8659577e018d3e3a07c2f912355ee8fc29
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2019-10-07 00:41:47 -0500

    Update update_examples_1.py

--- English/update_successions/update_examples_1.py
+++ English/update_successions/update_examples_1.py
@@ -269,3 +269,24 @@ class SliderCrankMechanism(Scene):
         piston = Rectangle(color=color, height=1, witdh=1.5)\
                  .set_fill(color,0.7).scale(0.7).move_to(origin+RIGHT * d + UP*c)
         return piston
+    
+class TriangleScene(Scene):
+    def construct(self):
+        circle = Circle(radius=3)
+        base_line = Line(ORIGIN,RIGHT*3,color=ORANGE)
+        side_1 = Line(ORIGIN,RIGHT*3,color=BLUE)
+        side_2 = Line(RIGHT*3,RIGHT*3,color=PURPLE)
+        sides = VGroup(side_1,side_2)
+        
+        def triangle_update(mob):
+            side_1,side_2 = mob
+            new_side_1 = Line(ORIGIN,circle.get_end(),color=BLUE)
+            new_side_2 = Line(RIGHT*3,circle.get_end(),color=PURPLE)
+            side_1.become(new_side_1)
+            side_2.become(new_side_2)
+
+        sides.add_updater(triangle_update)
+        self.add(base_line,sides)
+        self.play(ShowCreation(circle,run_time=3))
+
+        self.wait()
