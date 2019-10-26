d00561ca33ee3cbcbadd753b89cd1f2f3fd9c779
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2019-10-26 00:18:52 -0500

    Create paths.py

---
+++ English/extra/faqs/paths.py
@@ -0,0 +1,208 @@
+#from manimlib.imports import *
+#from big_ol_pile_of_manim_imports import *
+
+class Grid(VGroup):
+    CONFIG = {
+        "height": 6.0,
+        "width": 6.0,
+    }
+
+    def __init__(self, rows, columns, **kwargs):
+        digest_config(self, kwargs, locals())
+        super().__init__(**kwargs)
+
+        x_step = self.width / self.columns
+        y_step = self.height / self.rows
+
+        for x in np.arange(0, self.width + x_step, x_step):
+            self.add(Line(
+                [x - self.width / 2., -self.height / 2., 0],
+                [x - self.width / 2., self.height / 2., 0],
+            ))
+        for y in np.arange(0, self.height + y_step, y_step):
+            self.add(Line(
+                [-self.width / 2., y - self.height / 2., 0],
+                [self.width / 2., y - self.height / 2., 0]
+            ))
+
+
+class ScreenGrid(VGroup):
+    CONFIG = {
+        "rows": 8,
+        "columns": 14,
+        "height": FRAME_Y_RADIUS * 2,
+        "width": 14,
+        "grid_stroke": 0.5,
+        "grid_color": WHITE,
+        "axis_color": RED,
+        "axis_stroke": 2,
+        "labels_scale": 0.5,
+        "labels_buff": 0,
+        "number_decimals": 2
+    }
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        rows = self.rows
+        columns = self.columns
+        grid = Grid(width=self.width, height=self.height, rows=rows, columns=columns)
+        grid.set_stroke(self.grid_color, self.grid_stroke)
+
+        vector_ii = ORIGIN + np.array((- self.width / 2, - self.height / 2, 0))
+        vector_si = ORIGIN + np.array((- self.width / 2, self.height / 2, 0))
+        vector_sd = ORIGIN + np.array((self.width / 2, self.height / 2, 0))
+
+        axes_x = Line(LEFT * self.width / 2, RIGHT * self.width / 2)
+        axes_y = Line(DOWN * self.height / 2, UP * self.height / 2)
+
+        axes = VGroup(axes_x, axes_y).set_stroke(self.axis_color, self.axis_stroke)
+
+        divisions_x = self.width / columns
+        divisions_y = self.height / rows
+
+        directions_buff_x = [UP, DOWN]
+        directions_buff_y = [RIGHT, LEFT]
+        dd_buff = [directions_buff_x, directions_buff_y]
+        vectors_init_x = [vector_ii, vector_si]
+        vectors_init_y = [vector_si, vector_sd]
+        vectors_init = [vectors_init_x, vectors_init_y]
+        divisions = [divisions_x, divisions_y]
+        orientations = [RIGHT, DOWN]
+        labels = VGroup()
+        set_changes = zip([columns, rows], divisions, orientations, [0, 1], vectors_init, dd_buff)
+        for c_and_r, division, orientation, coord, vi_c, d_buff in set_changes:
+            for i in range(1, c_and_r):
+                for v_i, directions_buff in zip(vi_c, d_buff):
+                    ubication = v_i + orientation * division * i
+                    coord_point = round(ubication[coord], self.number_decimals)
+                    label = TextMobject(f"{coord_point}").scale(self.labels_scale)
+                    label.next_to(ubication, directions_buff, buff=self.labels_buff)
+                    labels.add(label)
+
+        self.add(grid, axes, labels)
+
+
+def coord(x,y,z=0):
+    return np.array([x,y,z])
+
+def getX(mob):
+    return mob.get_center()[0]
+
+def getY(mob):
+    return mob.get_center()[1]
+
+# Abstract class:
+class PathScene(Scene):
+    CONFIG = {
+        "x_coords":[0,  1, 3,  -2, -3],
+        "y_coords":[3, -2, 1, 2.5, -1]
+    }
+    """
+    The setup method it is executed before the construct method, 
+    so whatever they write in the setup method will be executed 
+    before the construct method
+    """
+    def setup(self):
+        self.screen_grid = ScreenGrid()
+        # tuples = [(0,3),(1,-2)...]
+        self.tuples = list(zip(self.x_coords,self.y_coords))
+
+        dots,labels,numbers = self.get_all_mobs()
+        self.add(self.screen_grid,dots,labels,numbers)
+
+    def get_dots(self,coords):
+        # This is called list comprehension, learn to use it here:
+        # https://www.youtube.com/watch?v=AhSvKGTh28Q
+        dots = VGroup(*[Dot(coord(x,y)) for x,y in coords])
+        return dots
+
+    def get_dot_labels(self,dots,direction=RIGHT):
+        labels = VGroup(*[
+            # This is called f-strings, learn to use it here:
+            # https://www.geeksforgeeks.org/formatted-string-literals-f-strings-python/
+            TexMobject(f"({getX(dot)},{getY(dot)})",height=0.3)\
+                      .next_to(dot,direction,buff=SMALL_BUFF) 
+                      # This is called Multi-line statement, learn how to use it here:
+                      # https://www.programiz.com/python-programming/statement-indentation-comments
+            for dot in dots
+            ])
+        return labels
+
+    def get_dot_numbers(self,dots):
+        dots = self.get_dots(self.tuples)
+        numbers = VGroup(*[
+            TextMobject(f"{n}",height=0.2).next_to(dot,DOWN,buff=SMALL_BUFF)
+            for n,dot in zip(range(1,len(dots)+1),dots)
+            ])
+        return numbers
+
+    def get_all_mobs(self):
+        dots = self.get_dots(self.tuples)
+        labels = self.get_dot_labels(dots)
+        numbers = self.get_dot_numbers(dots)
+        return dots,labels,numbers
+
+class ShowPoints(PathScene):
+    pass
+
+class PathAsCorners(PathScene):
+    def construct(self):
+        path = VMobject()
+        path.set_points_as_corners([*[coord(x,y) for x,y in self.tuples]])
+        self.add(path)
+
+class PathSmoothly(PathScene):
+    def construct(self):
+        path = VMobject()
+        path.set_points_smoothly([*[coord(x,y) for x,y in self.tuples]])
+        self.add(path)
+
+class PathPoints(PathScene):
+    def construct(self):
+        path = VMobject()
+        path.set_points_smoothly([*[coord(x,y) for x,y in self.tuples]])
+        bezier_points = VGroup(*[Dot(coord,color=RED) for coord in path.points])
+        self.add(path,bezier_points)
+
+class AppendPoints(PathScene):
+    def construct(self):
+        path = VMobject()
+        path.set_points_as_corners([*[coord(x,y) for x,y in self.tuples]])
+        self.add(path)
+        self.wait()
+        new_points = np.array([coord(-5,1),coord(-1,1)])
+        new_dots = self.get_dots(new_points[:,:2])
+        """       Special atention to this: ----
+        This is an slice, see: https://railsware.com/blog/python-for-machine-learning-indexing-and-slicing-for-lists-tuples-strings-and-other-sequential-types/
+        new_points = 
+        [   Columns:
+             0   1  2
+            [-5  1  0]   <- Row 0
+            [-1  1  0]   <- Row 1
+                        ]
+        So, the first ":" means all rows
+        The ":2" means only take the elements from the first column to the second one,
+        that is, the columns 0 and 1
+        """
+        new_labels = self.get_dot_labels(new_dots,UP)
+        path.become(
+            VMobject().set_points_as_corners([*path.points,*new_points])
+        )
+        # The most recent version have new methods that can do this more easy.
+        VGroup(new_dots,new_labels).set_color(TEAL)
+        self.add(new_dots,new_labels)
+        self.wait(2)
+
+class TransformPathStyle(PathScene):
+    def construct(self):
+        path = VMobject()
+        path.set_points_as_corners([*[coord(x,y) for x,y in self.tuples]])
+        self.add(path)
+        self.play(path.make_smooth)
+        self.wait()
+        """
+        There are 3 methods:
+            path.make_smooth()
+            path.make_jagged()
+            path.change_anchor_mode()
+        """
