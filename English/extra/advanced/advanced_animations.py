619716311f3ef94446153288adbe96d2305b16ae
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2020-02-08 15:08:14 -0600

    Update advanced_animations.py

--- English/extra/advanced/advanced_animations.py
+++ English/extra/advanced/advanced_animations.py
@@ -268,3 +268,63 @@ class RotationAndMoveFail(Scene):
                 run_time=4
             )
         
+class RotateWithPath(Scene):
+    def construct(self):
+        square1, square2 = VGroup(
+                Square(color=RED), Square(color=BLUE)
+            ).scale(0.5).set_x(-5)
+
+        path = Line(LEFT*5,RIGHT*5,stroke_opatity=0.5)
+        path.points[1:3] += UP*2
+
+        square2.save_state()
+        def update_rotate_move(mob,alpha):
+            square2.restore()
+            square2.move_to(path.point_from_proportion(alpha))
+            square2.rotate(3*PI*alpha)
+
+        self.add(square1,square2,path)
+        self.play(
+                MoveAlongPath(square1,path),
+                Rotate(square1,2*PI/3,about_point=square1.get_center()),
+                UpdateFromAlphaFunc(square2,update_rotate_move),
+                run_time=4
+            )
+        self.wait()
+
+class MoveAlongPathWithAngle(Scene):
+    def get_pendient(self,path,proportion,dx=0.01):
+        if proportion < 1:
+            coord_i = path.point_from_proportion(proportion)
+            coord_f = path.point_from_proportion(proportion+dx)
+        else:
+            coord_i = path.point_from_proportion(proportion-dx)
+            coord_f = path.point_from_proportion(proportion)
+        line = Line(coord_i,coord_f)
+        angle = line.get_angle()
+        return angle
+
+    def construct(self):
+        path = Line(LEFT*5,RIGHT*5,stroke_opatity=0.5)
+        path.points[1:3] += UP*2
+        start_angle = self.get_pendient(path,0)
+
+        triangle = Triangle().set_height(0.5)
+        triangle.move_to(path.get_start())
+        triangle.rotate(-PI/2)
+        triangle.save_state()
+        triangle.rotate(start_angle,about_point=triangle.get_center())
+
+
+        def update_rotate_move(mob,alpha):
+            triangle.restore()
+            angle = self.get_pendient(path,alpha)
+            triangle.move_to(path.point_from_proportion(alpha))
+            triangle.rotate(angle,about_point=triangle.get_center())
+
+        self.add(triangle,path)
+        self.play(
+                UpdateFromAlphaFunc(triangle,update_rotate_move),
+                run_time=4
+            )
+        self.wait()
