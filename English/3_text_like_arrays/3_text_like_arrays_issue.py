dd3e92276f396cc922a7628d0c3342736d3ce676
NavpreetDevpuri <30471072+NavpreetDevpuri@users.noreply.github.com>
2020-05-03 02:08:40 +0530

    Create 3_text_like_arrays_issue.py

---
+++ English/3_text_like_arrays/3_text_like_arrays_issue.py
@@ -0,0 +1,16 @@
+class colorIssue(Scene):
+    def construct(self):
+        # it shows error if you put a single "{" or "}" braces character into array 
+        numeros4 = TexMobject( "\\mathbb{N}", "=", "\\{", "1", ",", "2", ",", "3", ",", "\\ldots", "\\}" )
+        numeros4[0][2].set_color(RED)
+        self.add(numeros4)
+        self.wait()
+
+class colorissueSolved(Scene):
+    def construct(self):
+        # if you want to color "{" or "}" braces character separately
+        numeros4 = TexMobject( "\\mathbb{N}=\\{1,2,3,\\ldots\\}")
+        numeros4[0][0:2].set_color(GREEN)
+        numeros4[0][2].set_color(RED)
+        self.add(numeros4)
+        self.wait()
