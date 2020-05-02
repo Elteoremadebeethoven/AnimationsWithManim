f2a0da93af469abf4ce74221624b812a1c6de215
NavpreetDevpuri <30471072+NavpreetDevpuri@users.noreply.github.com>
2020-05-03 02:15:46 +0530

    Update 3_text_like_arrays_better_approach.py

--- English/3_text_like_arrays/3_text_like_arrays_better_approach.py
+++ English/3_text_like_arrays/3_text_like_arrays_better_approach.py
@@ -10,7 +10,9 @@ class colorissueSolved(Scene):
     def construct(self):
         # if you want to color "{" or "}" braces character separately
         numeros4 = TexMobject( "\\mathbb{N}=\\{1,2,3,\\ldots\\}")
+        # coloring first 2 characters 
         numeros4[0][0:2].set_color(GREEN)
+        # coloring 3rd character
         numeros4[0][2].set_color(RED)
         self.add(numeros4)
         self.wait()
